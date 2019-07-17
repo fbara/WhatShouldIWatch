3bc4c39d600dcc7a22a229064b130c1fd33d8cfa
Frank S. Bara <fbara1@yahoo.com>
2019-07-17 13:00:57 -0500

    Added movie search function.

--- WhatShouldIWatch/MovieView.swift
+++ WhatShouldIWatch/MovieView.swift
@@ -9,26 +9,55 @@
 import SwiftUI
 
 struct MovieView : View {
-    var movies: [Movie] = []
+    @State var movies: [Movie] = []
+    @State var searchTerm = ""
     
     var body: some View {
-        List(movies) { movie in
+        VStack {
             HStack {
-                Image(uiImage: movie.link.downloadCoverImage())
-                .resizable()
-                    .frame(width: 87, height: 127)
-                .cornerRadius(10)
-                Text(movie.title)
+                TextField($searchTerm, placeholder: Text("Search...")) {
+                    self.processSearch()
+                    UIApplication.shared.keyWindow?.endEditing(true)
+                }
+                Image(systemName: "xmark.circle.fill")
+                    .tapAction {
+                        self.searchTerm = ""
+                }
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            List(movies) { movie in
+                HStack {
+                    Image(uiImage: movie.link.downloadCoverImage())
+                        .resizable()
+                        .frame(width: 87, height: 127)
+                        .cornerRadius(10)
+                        VStack {
+                            ScrollView {
+                                Text(movie.title)
+                                    .font(.largeTitle)
+                            }
+                            // TODO: Rating
+                        }
+                    
+                    }
+                }
             }
-        }
 
     }
+    
+    private func processSearch() {
+        if searchTerm == "" {
+            movies = MovieDataProvider.shared.ratedMovies()
+        } else {
+            movies = MovieDataProvider.shared.performSearch(for: searchTerm)
+        }
+    }
 }
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        MovieView(movies: [MovieDataProvider.shared.movies[0]])
+        MovieView(movies: [MovieDataProvider.shared.movies[1]])
     }
 }
 #endif
--- WhatShouldIWatch/SceneDelegate.swift
+++ WhatShouldIWatch/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: MovieView(movies: [MovieDataProvider.shared.movies[0]]))
+            window.rootViewController = UIHostingController(rootView: MovieView())
             self.window = window
             window.makeKeyAndVisible()
         }
